ffbc6124828bf2ce7a8c78d00d84b1b4ac49f4fa
Yash Verma <yeshverma5122@gmail.com>
2025-06-11 23:40:32 +0530

    Auth UI

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import { SignInView } from "@/modules/sign-in-view";
+
+const Page = () => {
+    return <SignInView />
+}
+
+export default Page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import { SignUpView } from "@/modules/sign-up-view";
+
+const Page = () => {
+    return <SignUpView />
+}
+
+export default Page;
